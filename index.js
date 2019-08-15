4b78ca42ad69dab45e6c89cf679414bd1e0f7dd7
M Banks <montensia1@outlook.com>
2019-08-15 01:22:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Code your solutions in this file
 
 function writeCards(str_arr, event){
+  let result = [];
   for(let i = 0; i < str_arr.length; i++){
-    
+    `Thank you, ${str_arr[i],}, for the wonderful birthday gift.`
   }
+  return result;
 }
 
 writeCards
